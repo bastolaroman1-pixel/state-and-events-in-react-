0b4fb7daa2d0585c039fade8b19d1cfc98392a38
bastolaroman1-pixel <bastolaroman1@gmail.com>
2026-06-17 21:48:34 +0545

    some states and event

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import CounterApp from './CounterApp'
 import InputText from './RealTime'
 import Toggle from './Toggle'
-import Theme from './Theme'
+
 import ThemeToggle from './Theme'
 
 
